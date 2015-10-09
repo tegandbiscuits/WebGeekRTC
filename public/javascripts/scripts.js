9a11beb4e1f5683dbc6e3edc2dbdd8b6397fc8ef
Nate Rauh <NRauh@users.noreply.github.com>
2015-10-09 01:32:18 -0500

    Cleaned up code a bit. Also forgot in last js commit, can hang up.

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -10,25 +10,26 @@ $(function () {
 														navigator.mozGetUserMedia ||
 														navigator.msGetUserMedia;
 
-	navigator.getUserMedia({audio: false, video: true},
-		function(stream) {
-			var video = document.querySelector("#my-cam");
-			myFeed = stream;
-			video.src = URL.createObjectURL(stream);
-			video.onloadedmetadata = function(e) {
-				video.play();
-			};
-		}, function(err) {
-			console.log("Error", err);
-		}
-	);
+
+	// Getting the user feed (to be sent and self monitor)
+	navigator.getUserMedia({
+		audio: false,
+		video: true
+	}, function(stream) {
+		var video = document.querySelector("#my-cam");
+		myFeed = stream;
+		video.src = URL.createObjectURL(stream);
+	}, function(err) {
+		console.log("Error", err);
+	});
 
 
+	// Caller functions
 	$("#call").click(function() {
-		// this is for the caller
 		var connectPeer = prompt("Enter ID to connect to");
 		var call = peer.call(connectPeer, myFeed);
 		var dataConn = peer.connect(connectPeer);
+
 		displayCall(call);
 		canHangup(call);
 
@@ -37,36 +38,38 @@ $(function () {
 		});
 
 		$("#textchat").click(function() {
-			dataConn.send("Hello world");
+			sendMessage(dataConn, "Hello World");
 		});
 	});
 
+
+	// Called functions
 	peer.on("call", function(call) {
-		// this is for the called
 		console.log("Getting a call");
 		call.answer(myFeed);
+
 		displayCall(call);
 		canHangup(call);
+
 		peer.on("connection", function(dataConn) {
 			dataConn.on("data", function(data) {
 				console.log(data);
 			});
 
 			$("#textchat").click(function() {
-				dataConn.send("Hey Buddy");
+				sendMessage(dataConn, "Hey Buddy");
 			});
 		});
 	});
+
 
 	function displayCall(call) {
 		call.on("stream", function(stream) {
 			var callCam = document.querySelector("#call-cam");
 			callCam.src = URL.createObjectURL(stream);
-			callCam.onloadedmetadata = function(e) {
-				callCam.play();
-			};
 		});
 	}
+
 
 	function canHangup(call) {
 		$("#hangup").click(function() {
@@ -74,9 +77,11 @@ $(function () {
 		});
 	}
 
+
 	function sendMessage(dataConn, msg) {
 		dataConn.send(msg);
 	}
+
 
 	peer.on("error", function(err) {
 		if (err.type === "unavailable-id") {
@@ -84,5 +89,4 @@ $(function () {
 			peer = new Peer(peerId, {key: "vpzfzavxowjc3di"});
 		}
 	});
-
-})
+});
